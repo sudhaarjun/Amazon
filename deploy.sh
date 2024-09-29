66cb7f90a4a74b4baf0b5623af3d0f328924fa30
sudhaeee <sudha51008@gmail.com>
2024-09-29 12:01:38 +0530

    commits

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ Hello this is the deployment script...
 This script is modified..
 this is the deployemt stage..
 deployment script is there in that file..
+this is the only deployment script
--- string.sh
+++ string.sh
@@ -1,3 +1,4 @@
 Hello this file contains the string type
 this is contains the shellscript file..
+this contains only the strings..
 shellscritps are there in that file
